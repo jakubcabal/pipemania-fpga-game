901188e0a1daa3d3e6bc856048cf5a78ee890de1
Jakub Cabal <jakubcabal@gmail.com>
2016-07-24 19:37:15 +0200

    FALLING_EDGE_DETECTOR: Clean code

--- source/detectors/falling_edge/falling_edge_detector.vhd
+++ source/detectors/falling_edge/falling_edge_detector.vhd
@@ -1,36 +1,39 @@
--- falling_edge_detector.vhd - Modul pro detekci sestupne hrany
--- Autori: Jakub Cabal
--- Posledni zmena: 02.10.2014 19:45
--- Popis: Tato jednotka detekuje sestupnou hranu vstupniho signalu
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- PROJECT: PIPE MANIA - GAME FOR FPGA
+--------------------------------------------------------------------------------
+-- NAME:    FALLING_EDGE_DETECTOR
+-- AUTHORS: Jakub Cabal <xcabal05@stud.feec.vutbr.cz>
+-- LICENSE: The MIT License, please read LICENSE file
+-- WEBSITE: https://github.com/jakubcabal/pipemania-fpga-game
+--------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity FALLING_EDGE_DETECTOR is
-   Port (
-      CLK    : in   STD_LOGIC; -- Vychozi hodinovy signal
-      VSTUP  : in   STD_LOGIC; -- Vstup na detekci sestupne hrany
-      VYSTUP : out  STD_LOGIC  -- Vystup je aktivni pokud byla detekovana sestupna hrana
-   );
+    Port(
+        CLK    : in  std_logic; -- Hodinovy signal
+        VSTUP  : in  std_logic; -- Vstup na detekci sestupne hrany
+        VYSTUP : out std_logic  -- Aktivni, pokud byla detekovana sestupna hrana
+    );
 end FALLING_EDGE_DETECTOR;
 
 architecture FULL of FALLING_EDGE_DETECTOR is
 
-   signal predchozi_vstup : STD_LOGIC;
-	signal sig_vystup      : STD_LOGIC;
+    signal predchozi_vstup : std_logic;
+    signal sig_vystup      : std_logic;
 
 begin
-   
-   -- Registr, ktery uchovava hodnotu vstupu z predchoziho taktu
-   process(CLK)
-   begin
-      if(rising_edge(CLK)) then
-         predchozi_vstup <= VSTUP;
-			VYSTUP          <= sig_vystup;
-      end if;
-   end process;
 
-   sig_vystup <= NOT vstup AND predchozi_vstup;
+    -- Registr, ktery uchovava hodnotu vstupu z predchoziho taktu
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            predchozi_vstup <= VSTUP;
+            VYSTUP          <= sig_vystup;
+        end if;
+    end process;
+
+    sig_vystup <= NOT vstup AND predchozi_vstup;
 
 end FULL;
