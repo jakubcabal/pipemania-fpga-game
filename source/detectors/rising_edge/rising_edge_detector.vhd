36f3312d2e946a9c96cb0cd11d27ba96f8217913
Jakub Cabal <jakubcabal@gmail.com>
2016-07-24 19:47:10 +0200

    RISING_EDGE_DETECTOR: Clean code

--- source/detectors/rising_edge/rising_edge_detector.vhd
+++ source/detectors/rising_edge/rising_edge_detector.vhd
@@ -1,33 +1,40 @@
--- rising_edge_detector.vhd - Modul pro detekci nastupne hrany
--- Autori: Tomas Bannert
--- Posledni zmena: 02.10.2014 21:10
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- PROJECT: PIPE MANIA - GAME FOR FPGA
+--------------------------------------------------------------------------------
+-- NAME:    RISING_EDGE_DETECTOR
+-- AUTHORS: Tomáš Bannert <xbanne00@stud.feec.vutbr.cz>
+-- LICENSE: The MIT License, please read LICENSE file
+-- WEBSITE: https://github.com/jakubcabal/pipemania-fpga-game
+--------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity rising_edge_detector is
-    Port ( VSTUP  : in  STD_LOGIC; 	-- VSTUP
-           CLK    : in  STD_LOGIC;	-- HODINOVY SIGNAL
-           VYSTUP : out  STD_LOGIC);	-- VYSTUP
-end rising_edge_detector;
+entity RISING_EDGE_DETECTOR is
+    Port(
+        CLK    : in  std_logic; -- HODINOVY SIGNAL
+        VSTUP  : in  std_logic; -- VSTUP
+        VYSTUP : out std_logic  -- VYSTUP
+    );
+end RISING_EDGE_DETECTOR;
 
-architecture Behavioral of rising_edge_detector is
+architecture Behavioral of RISING_EDGE_DETECTOR is
 
-signal A : STD_LOGIC;			-- POMOCNY SIGNAL
-signal B : STD_LOGIC;			-- POMOCNY SIGNAL
-signal C : STD_LOGIC;			-- POMOCNY SIGNAL
+    signal a : std_logic; -- POMOCNY SIGNAL
+    signal b : std_logic; -- POMOCNY SIGNAL
+    signal c : std_logic; -- POMOCNY SIGNAL
 
 begin
 
-process(CLK)				-- 2 D FLIP-FLOPS
-begin
-	if(rising_edge(CLK)) then
-		A <= VSTUP;
-		VYSTUP <= B;
-	end if;
-end process;
+    process (CLK) -- 2 D FLIP-FLOPS
+    begin
+        if(rising_edge(CLK)) then
+            a <= VSTUP;
+            VYSTUP <= b;
+        end if;
+    end process;
 
-C <= VSTUP XOR A;			-- XOR GATE
-B <= VSTUP AND C;			-- AND GATE
+    c <= VSTUP XOR a; -- XOR GATE
+    b <= VSTUP AND c; -- AND GATE
 
 end Behavioral;
--- source/detectors/rising_edge/rising_edge_detector_test_bench.vhd
+++ source/detectors/rising_edge/rising_edge_detector_test_bench.vhd
@@ -1,59 +1,57 @@
--- falling_edge_detector.vhd - Modul pro detekci sestupne hrany
--- Autori: Tom� Bannert
--- Posledni zmena: 02.10.2014 21:20
-----------------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+--------------------------------------------------------------------------------
+-- PROJECT: PIPE MANIA - GAME FOR FPGA
+--------------------------------------------------------------------------------
+-- NAME:    RISING_EDGE_DETECTOR_TB
+-- AUTHORS: Tomáš Bannert <xbanne00@stud.feec.vutbr.cz>
+-- LICENSE: The MIT License, please read LICENSE file
+-- WEBSITE: https://github.com/jakubcabal/pipemania-fpga-game
+--------------------------------------------------------------------------------
 
- 
-ENTITY rising_edge_detector_TB IS
-END rising_edge_detector_TB;
- 
-ARCHITECTURE behavior OF rising_edge_detector_TB IS 
- 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity RISING_EDGE_DETECTOR_TB is
+end RISING_EDGE_DETECTOR_TB;
+
+architecture behavior of RISING_EDGE_DETECTOR_TB is
+
     -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT rising_edge_detector
-    PORT(
-         VSTUP  : IN  std_logic;
-         CLK    : IN  std_logic;
-         VYSTUP : OUT  std_logic
-        );
-    END COMPONENT;
-    
+    component RISING_EDGE_DETECTOR
+    port(
+        VSTUP  : in  std_logic;
+        CLK    : in  std_logic;
+        VYSTUP : out std_logic
+    );
+    end component;
 
-   --Inputs
-   signal VSTUP  : std_logic := '0';
-   signal CLK    : std_logic := '0';
+    -- Inputs
+    signal VSTUP  : std_logic := '0';
+    signal CLK    : std_logic := '0';
 
-   --Outputs
-   signal VYSTUP : std_logic;
- 
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: rising_edge_detector PORT MAP (
-          VSTUP => VSTUP,
-          CLK => CLK,
-          VYSTUP => VYSTUP
-        );
+    -- Outputs
+    signal VYSTUP : std_logic;
 
+begin
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-		wait for 100 ns;	
-			VSTUP <= '0';
-		wait for 100 ns;
-			VSTUP <= '1';
-		wait for 100 ns;	
-			VSTUP <= '0';
-		wait for 100 ns;
-			VSTUP <= '1';
-      -- insert stimulus here 
+    -- Instantiate the Unit Under Test (UUT)
+    uut: RISING_EDGE_DETECTOR
+    port map(
+        VSTUP  => VSTUP,
+        CLK    => CLK,
+        VYSTUP => VYSTUP
+    );
 
-      wait;
-   end process;
+    -- Stimulus process
+    stim_proc: process
+    begin
+        VSTUP <= '0';
+        wait for 100 ns;
+        VSTUP <= '1';
+        wait for 100 ns;
+        VSTUP <= '0';
+        wait for 100 ns;
+        VSTUP <= '1';
+        wait;
+    end process;
 
-END;
+end;
