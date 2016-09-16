7f97a8fc117939cd6c2b6a1b8aabbe81f3a527a4
Jakub Cabal <jakubcabal@gmail.com>
2016-09-16 21:46:29 +0200

    PS2_RX: Clean code, use async reset

--- source/comp/control/ps2_rx.vhd
+++ source/comp/control/ps2_rx.vhd
@@ -1,32 +1,36 @@
--- ps2_rx.vhd - Modul pro prijem signalu z portu PS2
--- Autori: Jakub Cabal
--- Posledni zmena: 04.10.2014
--- Popis: Tato jednotka zajistuje prijem dat z PS2 portu v cetne kontroly parity
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- PROJECT: PIPE MANIA - GAME FOR FPGA
+--------------------------------------------------------------------------------
+-- NAME:    PS2_RX
+-- AUTHORS: Jakub Cabal <xcabal05@stud.feec.vutbr.cz>
+-- LICENSE: The MIT License, please read LICENSE file
+-- WEBSITE: https://github.com/jakubcabal/pipemania-fpga-game
+--------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity PS2_RX is
-   Port (
-      CLK          : in   STD_LOGIC; -- Vychozi hodinovy signal
-      RST          : in   STD_LOGIC; -- Vychozi synchronni reset
-      PS2C         : in   STD_LOGIC; -- Hodinovy signal z PS2 portu
-      PS2D         : in   STD_LOGIC; -- Seriova vstupni data z PS2 portu
-      PS2RX_DATA   : out  STD_LOGIC_VECTOR(7 downto 0); -- Vystupni data
-      PS2RX_VALID  : out  STD_LOGIC  -- Data jsou pripravena na vycteni
-   );
+    Port (
+        CLK          : in  std_logic; -- Vychozi hodinovy signal
+        RST          : in  std_logic; -- Vychozi reset
+        PS2C         : in  std_logic; -- Hodinovy signal z PS2 portu
+        PS2D         : in  std_logic; -- Seriova vstupni data z PS2 portu
+        PS2RX_DATA   : out std_logic_vector(7 downto 0); -- Vystupni data
+        PS2RX_VALID  : out std_logic  -- Data jsou pripravena na vycteni
+    );
 end PS2_RX;
 
 architecture FULL of PS2_RX is
 
-   signal ps2_valid       : STD_LOGIC;
-   signal parity_valid    : STD_LOGIC;
-   signal parity_ctrl     : STD_LOGIC;
-   signal parity_ps2      : STD_LOGIC;
-   signal ps2_bit_count   : STD_LOGIC_VECTOR(3 downto 0);
-   signal sig_ps2rx_data  : STD_LOGIC_VECTOR(7 downto 0);
-   signal sig_ps2rx_data2 : STD_LOGIC_VECTOR(7 downto 0);
+   signal ps2_valid       : std_logic;
+   signal parity_valid    : std_logic;
+   signal parity_ctrl     : std_logic;
+   signal parity_ps2      : std_logic;
+   signal ps2_bit_count   : unsigned(3 downto 0);
+   signal sig_ps2rx_data  : std_logic_vector(7 downto 0);
+   signal sig_ps2rx_data2 : std_logic_vector(7 downto 0);
 
    type state is (idle, dps, load);
    signal present_st : state;
@@ -34,142 +38,148 @@ architecture FULL of PS2_RX is
 
 begin
 
-   -- Detektor sestupne hrany
-   falling_edge_detector_1: entity work.FALLING_EDGE_DETECTOR
-   port map(
-      CLK    => CLK,
-      VSTUP  => PS2C,
-      VYSTUP => ps2_valid  -- Pri sestupne hrane jsou validni data
-   );
+    ----------------------------------------------------------------------------
+    -- FALLING EDGE DETECTOR OF PS/2 CLOCK
+    ----------------------------------------------------------------------------
 
-   ----------------------------------------------------------------
-   -- STAVOVY AUTOMAT
-   ----------------------------------------------------------------
+    falling_edge_detector_i : entity work.FALLING_EDGE_DETECTOR
+    port map(
+        CLK    => CLK,
+        VSTUP  => PS2C,
+        VYSTUP => ps2_valid  -- Pri sestupne hrane jsou validni data
+    );
 
-   -- Pametova cast stavoveho automatu
-   process (CLK) 
-   begin
-      if (rising_edge(CLK)) then
-         if (RST = '1') then
+    ----------------------------------------------------------------------------
+    -- PS2 RX FSM
+    ----------------------------------------------------------------------------
+
+    fsm_reg : process (CLK, RST)
+    begin
+        if (RST = '1') then
             present_st <= idle;
-         else
+        elsif (rising_edge(CLK)) then
             present_st <= next_st;
-         end if;
-      end if;   
-   end process;
+        end if;
+    end process;
 
-   -- Rozhodovaci cast stavoveho automatu
-   process (present_st, PS2D, ps2_valid, ps2_bit_count)
-   begin
-      case present_st is
-     
-         when idle =>
-            if (ps2_valid = '1' AND PS2D = '0') then
-               next_st <= dps;
-            else
-               next_st <= idle;
-            end if;
+    -- Rozhodovaci cast stavoveho automatu
+    process (present_st, PS2D, ps2_valid, ps2_bit_count)
+    begin
+        case present_st is
 
-         when dps =>
-            if (ps2_bit_count = 11) then
-               next_st <= load;
-            else
-               next_st <= dps;
-            end if;
+            when idle =>
+                if (ps2_valid = '1' AND PS2D = '0') then
+                    next_st <= dps;
+                else
+                    next_st <= idle;
+                end if;
 
-         when load =>
-            next_st <= idle;
-         
-      end case;
-   end process;
+            when dps =>
+                if (to_integer(ps2_bit_count) = 11) then
+                    next_st <= load;
+                else
+                    next_st <= dps;
+                end if;
 
-   -- Vystupni cast stavoveho automatu
-   process (present_st, parity_valid)
-   begin
-      case present_st is
-     
-         when idle =>
-            PS2RX_VALID <= '0';
+            when load =>
+                next_st <= idle;
 
-         when dps =>
-            PS2RX_VALID <= '0';
+        end case;
+    end process;
 
-         when load =>
-            if (parity_valid = '1') then
-               PS2RX_VALID <= '1';
-            else
-               PS2RX_VALID <= '0';
-            end if;
-         
-      end case;
-   end process;
+    -- Vystupni cast stavoveho automatu
+    process (present_st, parity_valid)
+    begin
+        case present_st is
 
-   ----------------------------------------------------------------
-   -- PREVOD DAT
-   ----------------------------------------------------------------
+            when idle =>
+                PS2RX_VALID <= '0';
 
-   -- Citac poradi bitu v ramci
-   process (CLK) 
-   begin
-      if (rising_edge(CLK)) then
-         if (RST = '1') then 
+            when dps =>
+                PS2RX_VALID <= '0';
+
+            when load =>
+                PS2RX_VALID <= parity_valid;
+
+        end case;
+    end process;
+
+    ----------------------------------------------------------------------------
+    -- BIT COUNTER
+    ----------------------------------------------------------------------------
+
+    bit_cnt_p : process (CLK, RST)
+    begin
+        if (RST = '1') then
             ps2_bit_count <= (others => '0');
-         elsif (ps2_bit_count = 11) then  
-            ps2_bit_count <= (others => '0'); 
-         elsif (ps2_valid = '1') then
-            ps2_bit_count <= ps2_bit_count + 1;
-         end if;
-      end if;
-   end process; 
-
-   -- Cteni dat z PS2D
-   process (CLK) 
-   begin
-      if (rising_edge(CLK)) then
-         if (ps2_valid = '1') then 
-            if (ps2_bit_count = 9) then 
-               parity_ps2 <= PS2D;
-            elsif (ps2_bit_count > 0 AND ps2_bit_count < 9) then 
-               sig_ps2rx_data(6 downto 0) <= sig_ps2rx_data(7 downto 1);
-               sig_ps2rx_data(7) <= PS2D;
+        elsif (rising_edge(CLK)) then
+            if (to_integer(ps2_bit_count) = 11) then
+                ps2_bit_count <= (others => '0');
+            elsif (ps2_valid = '1') then
+                ps2_bit_count <= ps2_bit_count + 1;
             end if;
-         end if;
-      end if;
-   end process; 
+        end if;
+    end process;
 
-   -- Propagace PS2 dat na vystup
-   process (CLK) 
-   begin
-      if (rising_edge(CLK)) then
-         if (RST = '1') then 
-            sig_ps2rx_data2 <= (others => '0');
-         elsif (ps2_bit_count = 10) then
-            sig_ps2rx_data2 <= sig_ps2rx_data;
-         end if;
-      end if;
-   end process;
+    ----------------------------------------------------------------------------
+    -- PS/2 DATA
+    ----------------------------------------------------------------------------
 
-   PS2RX_DATA <= sig_ps2rx_data2; 
-   
-   ----------------------------------------------------------------
-   -- KONTROLA PARITY
-   ----------------------------------------------------------------
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (ps2_valid = '1') then
+                if (to_integer(ps2_bit_count) > 0 AND to_integer(ps2_bit_count) < 9) then
+                    sig_ps2rx_data(7 downto 0) <= PS2D & sig_ps2rx_data(7 downto 1);
+                end if;
+            end if;
+        end if;
+    end process;
 
-   parity_ctrl <= sig_ps2rx_data2(7) xor sig_ps2rx_data2(6) xor sig_ps2rx_data2(5) xor sig_ps2rx_data2(4)
-              xor sig_ps2rx_data2(3) xor sig_ps2rx_data2(2) xor sig_ps2rx_data2(1) xor sig_ps2rx_data2(0) xor '1';
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (ps2_valid = '1') then
+                if (to_integer(ps2_bit_count) = 9) then
+                    parity_ps2 <= PS2D;
+                end if;
+            end if;
+        end if;
+    end process;
 
-   -- Kontrola parity
-   process (CLK) 
-   begin
-      if (rising_edge(CLK)) then
-         if (RST = '1') then 
+    -- Propagace PS2 dat na vystup
+    process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (to_integer(ps2_bit_count) = 10) then
+                sig_ps2rx_data2 <= sig_ps2rx_data;
+            end if;
+        end if;
+    end process;
+
+    PS2RX_DATA <= sig_ps2rx_data2;
+
+    ----------------------------------------------------------------------------
+    -- DATA PARITY CHECK
+    ----------------------------------------------------------------------------
+
+    parity_ctrl <= sig_ps2rx_data2(7) xor sig_ps2rx_data2(6) xor
+                   sig_ps2rx_data2(5) xor sig_ps2rx_data2(4) xor
+                   sig_ps2rx_data2(3) xor sig_ps2rx_data2(2) xor
+                   sig_ps2rx_data2(1) xor sig_ps2rx_data2(0) xor '1';
+
+    -- Kontrola parity
+    process (CLK, RST)
+    begin
+        if (RST = '1') then
             parity_valid <= '0';
-         elsif (parity_ctrl = parity_ps2) then
-            parity_valid <= '1';
-         else
-            parity_valid <= '0';
-         end if;
-      end if;
-   end process;
+        elsif (rising_edge(CLK)) then
+            if (parity_ctrl = parity_ps2) then
+                parity_valid <= '1';
+            else
+                parity_valid <= '0';
+            end if;
+        end if;
+    end process;
 
 end FULL;
